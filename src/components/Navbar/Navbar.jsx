708f0af2e48ed1cbd4be0d953925384323cbfcf7
kwhong95 <hong-kyung-won@hong-gyeong-won-ui-MacBookAir.local>
2020-12-12 16:22:16 +0900

    STEP4. Add to Cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import { ShoppingFilled } from '@ant-design/icons';
  
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   return (
     <div className="header">
       <PageHeader
         avatar={{src:'https://e7.pngegg.com/pngimages/123/106/png-clipart-apple-logo-logo-apple-icon-information-apple-logo-logo-monochrome.png'}}
         title="Apple Sub Store"
         extra={[
-          <Badge count={2}>
+          <Badge count={totalItems}>
             <Button type="primary" icon={<ShoppingFilled />} />
           </Badge>
         ]}
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -4,14 +4,14 @@ import { ShoppingCartOutlined } from '@ant-design/icons'
 
 import './Product.css';
 
-const Product = ({ product }) => {
+const Product = ({ product, addToCart }) => {
   return (
     <div>
       <Card
         className="root"
         cover={<img className="media" alt="productImg" src={product.media.source} />}
         actions={[
-        <ShoppingCartOutlined className="cartButton" key="Add To Cart"/>
+        <ShoppingCartOutlined className="cartButton" key="Add To Cart" onClick={() => addToCart(product.id, 1)}/>
       ]}
       >
         <div className="cardTop"><h3>{product.name}</h3><h3>{product.price.formatted_with_symbol}</h3></div>
